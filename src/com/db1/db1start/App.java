8bc3aed27f381ce1df555d03b034e3c5cadd8fca
aly <alunos@intranet.lan>
2019-11-12 14:27:16 -0300

    att

--- src/com/db1/db1start/App.java
+++ src/com/db1/db1start/App.java
@@ -5,6 +5,53 @@ import java.util.Scanner;
 
 public class App {
 
+    public static Integer soma(Integer valor1, Integer valor2){
+        Integer resultado;
+        resultado = valor1 + valor2;
+
+        System.out.println("soma dos valores: " + resultado);
+        return resultado;
+    }
+
+    public static Integer subtra(Integer valor1, Integer valor2){
+        Integer result;
+        result = valor1 - valor2;
+        System.out.println("subtração dos valores: " + result);
+        return result;
+    }
+
+    public static String maiusc(String palav){
+        System.out.println("palavra em maiúsculo: " + palav.toUpperCase());
+        return palav;
+    }
+
+    public static String minusc(String palavra){
+        System.out.println("palavra em minúsculo: " + palavra.toLowerCase());
+        return palavra;
+    }
+
+    public static Double menor(Double val1, Double val2){
+        if (val1 < val2){
+            System.out.println("primeiro valor é menor que segundo valor");
+            return  val1;
+        } else {
+            System.out.println("segundo valor é menor que primeiro valor");
+            return val2;
+        }
+    }
+
+    public static Double menor3(Double val1, Double val2, Double val3){
+        if(val1 < val2 && val1 < val3){
+            System.out.println("primeiro valor é o menor: " + val1);
+            return val1;
+        } else if (val1 < val3 && val2 < val1){
+            System.out.println("segundo valor é menor: " + val2);
+            return val2;
+        } else {
+            System.out.println("terceiro valor é o menor: " + val3);
+            return val3;
+        }
+    }
 
     public static void main(String[] args) {
         Integer numero1, numero2, resultado;
@@ -17,14 +64,8 @@ public class App {
         System.out.println("insire outro número: ");
         Integer numero002 = scan.nextInt();
 
-        System.out.println("soma dos númeors: " +  (numero001 + numero002));
+        soma(numero001,numero002);
 
-       /*soma valores pre definidos
-        numero1 = 50;
-        numero2 = 40;
-        resultado = numero1 + numero2;
-        System.out.println(resultado);
-        */
 
         //subtração valores inseridos
         System.out.println("insira um número: ");
@@ -33,89 +74,36 @@ public class App {
         System.out.println("insire outro número: ");
         Integer numero200 = scan.nextInt();
 
-        System.out.println("subtração dos números: " + (numero100 - numero200));
+        subtra(numero100, numero200);
 
-         /* numeros pre definidos
-         Integer numero01, numero02, result;
-         numero01 = 30;
-         numero02 = 98;
-            result = numero01 - numero02;
-            System.out.println(result);
-        */
+
 
         //letra maiuscula com valores inseridos pelo usuário
         System.out.println("entre com uma palavra: ");
         String palavra = scan.next();
 
-        System.out.println(palavra.toUpperCase() + " em maiúsculo ");
+        maiusc(palavra);
 
         System.out.println("entre com uma palavra em maiúsculo: ");
-        String palavra1 = scan.next();
+            String palavra1 = scan.next();
+        minusc(palavra1);
 
-        System.out.println(palavra1.toLowerCase() + " em minúsculo ");
 
-        /*valores pre estabelecidos
-        String maiuscula;
-        maiuscula = "abcdc";
-        System.out.println(maiuscula.toUpperCase());
-
-        String minuscula;
-        minuscula = "ABCD";
-        System.out.println(minuscula.toLowerCase());
-        */
-
-        /*menor variavel
-        Double variavel1, variavel2;
-        variavel1 = 3.2;
-        variavel2 = 3.3;
-        if (variavel1 < variavel2){
-            System.out.println(variavel1 + "  é menor que " + variavel2);
-        } else {
-            System.out.println(variavel2 + " é o menor que "+ variavel1);
-        }
-        */
-
+        //menor valor
         System.out.println("digite o primeiro valor: ");
-        Double valor1 = scan.nextDouble();
+            Double valor1 = scan.nextDouble();
 
         System.out.println("digite o segundo valor: ");
-        Double valor2 = scan.nextDouble();
-        if (valor1 < valor2){
-            System.out.println("primeiro valor é menor que segundo valor");
-        } else {
-            System.out.println("segundo valor é menor que segundo valor");
-        }
+            Double valor2 = scan.nextDouble();
+        menor(valor1,valor2);
 
-
-
-        /*menor em tres
-        Double var1, var2, var3;
-        var1 = 5.5;
-        var2 = 6.4;
-        var3 = 9.2;
-        if (var1 < var2 && var1 < var3){
-            System.out.println(var1 + " é o menor número ");
-        }
-        if (var2 < var1 && var2 < var3){
-            System.out.println(var2 + " é o menor número ");
-        }
-        if (var3 < var1 && var3 < var2){
-            System.out.println(var3 + " é o menor número ");
-        }
-        */
-
+        //menor entre 3
         System.out.println("Insira o primeiro valor: ");
         Double primeiro = scan.nextDouble();
         System.out.println("Insira o segundo valor: ");
         Double segundo = scan.nextDouble();
         System.out.println("Insire o terceiro valor: ");
         Double terceiro = scan.nextDouble();
-        if(primeiro < segundo && primeiro < terceiro){
-            System.out.println("primeiro valor é o menor" + primeiro);
-        } else if (segundo < terceiro && segundo < primeiro){
-            System.out.println("segundo valor é menor" + segundo);
-        } else {
-            System.out.println("terceiro valor é o menor" + terceiro);
-        }
+        menor3(primeiro, segundo, terceiro);
     }
 }

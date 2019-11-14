e6eb4d72c507c5d14cee52d7f5dd8500fcf0dbd5
aly <alunos@intranet.lan>
2019-11-14 15:48:45 -0300

    att6

---
+++ src/com/db1/db1start/AulaEstruturaDeDados.java
@@ -0,0 +1,42 @@
+package com.db1.db1start;
+
+import javax.sound.midi.Soundbank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AulaEstruturaDeDados {
+
+    public static void main (String[] args) {
+
+        List<String> frutas = new ArrayList<>();
+        frutas.add("Banana");
+        frutas.add("Maçã");
+        frutas.add("Abacaxi");
+        frutas.add("Tomate");
+        frutas.add("Figo");
+        frutas.add("Morango");
+
+        frutas.addAll(Arrays.asList("Jaca", "Caju"));
+        System.out.println("Frutas inicias");
+        System.out.println(frutas);
+        System.out.println("------------------");
+
+        frutas.add("banana");
+        System.out.println(frutas.indexOf("Banana"));
+        System.out.println(frutas.lastIndexOf("Morango"));
+        System.out.println(frutas.size());
+
+        frutas.remove("Banana");
+        frutas.add(1,"Jambo");
+        System.out.println(frutas);
+
+        frutas.removeIf( f -> f.startsWith("J"));
+        System.out.println(frutas);
+
+        for(int i = 0; i < frutas.size(); i++){
+            System.out.println(frutas.get(i));
+        }
+    }
+}

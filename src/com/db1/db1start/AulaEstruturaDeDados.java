5d18cb3bff79f4e993f989dcbd9e4125124db423
aly <alunos@intranet.lan>
2019-11-14 17:00:46 -0300

    att8

--- src/com/db1/db1start/AulaEstruturaDeDados.java
+++ src/com/db1/db1start/AulaEstruturaDeDados.java
@@ -2,9 +2,7 @@ package com.db1.db1start;
 
 import javax.sound.midi.Soundbank;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class AulaEstruturaDeDados {
 
@@ -40,7 +38,7 @@ public class AulaEstruturaDeDados {
         }
 
         System.out.println("com for-each");
-        for(String s: frutas){
+        for(String a: frutas){
             System.out.println(frutas);
         }
 
@@ -48,8 +46,47 @@ public class AulaEstruturaDeDados {
         frutas.forEach(f -> System.out.println(f));
         frutas.forEach(System.out::println);
 
+        //1 metodo que retorne as cores que você mais gosta
+        //2 método que dado uma lista retorne a quantidade de itens
+        //3 método que receba 3 strings, adicione todos em uma lista e remova a segunda posição
+        //4 método que imprima as cores do primeiro método
+        //5 método que imprima as cores do primeiro método em ordem alfabética
+        //6 método que receba uma lista das cores que você mais gosta e o nome de uma cor a ser removida
+        //7 método que receba a lista de cores que você gosta e retorne e imprima em ordem decrescente (alfabética)
+        //8 método que receba uma lista de números  e retorne um mapa com listas de numeros pares e impares
+        //--------------------------------------------------------------------------------------------------------------
 
-        
+        //1
+        List<String> cores = new ArrayList<>();
+        cores.addAll(Arrays.asList("gray", "red", "blue", "black"));
+
+        //2
+        cores.size();
+
+        //3
+        List<String> stuff = new ArrayList<>();
+        stuff.addAll(Arrays.asList("ovo", "clara", "gema"));
+        stuff.remove(1);
+
+        //4
+        List<String> colors = cores;
+        System.out.println("printing colors " + colors);
+
+        //5
+        List<String> alfa = cores;
+        Collections.sort(alfa);
+        System.out.println(alfa);
+
+        //6
+        List<String> colorsilike = new ArrayList<>();
+        colorsilike.addAll(Arrays.asList("blue", "black", "red", "gray", "yellow"));
+        colorsilike.removeIf(s -> s.startsWith("y"));
+
+        //7
+        Collections.sort(colorsilike);
+        for(int i = 0; i < ; i--)
+
+
+
     }
-
 }

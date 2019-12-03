7f88856ca552d3ae3295bf40aac2371bbb213647
aly <alunos@intranet.lan>
2019-12-03 17:13:37 -0300

    vamo

--- Nova pasta/conta/src/com/db1/conta/NaoPodeSerMenorOuMair.java
+++
@@ -1,8 +0,0 @@
-package com.db1.conta;
-
-public class NaoPodeSerMenorOuMair extends RuntimeException{
-
-    public NaoPodeSerMenorOuMair(String frase){
-        super(frase);
-    }
-}
--- Nova pasta/src/com/db1/db1start/CampoNaoPodeSerNulo.java
+++
@@ -1,9 +0,0 @@
-package com.db1.db1start;
-
-public class CampoNaoPodeSerNulo extends RuntimeException {
-
-    public CampoNaoPodeSerNulo(String frase) {
-
-        super(frase);
-    }
-}
